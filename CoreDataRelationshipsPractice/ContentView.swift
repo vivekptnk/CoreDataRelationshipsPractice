42ab292338c655dd9bd9cbeac7671aa23ae047cf
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-17 00:12:55 -0400

    Set up done

--- CoreDataRelationshipsPractice/ContentView.swift
+++ CoreDataRelationshipsPractice/ContentView.swift
@@ -6,8 +6,42 @@
 //
 
 import SwiftUI
+import CoreData
+
+class CoreDataManager {
+    static let instance = CoreDataManager()
+    
+    let container : NSPersistentContainer
+    let context : NSManagedObjectContext
+    
+    init() {
+        container = NSPersistentContainer(name: "")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("error loading Core Data.\(error)")
+            }
+        }
+        context = container.viewContext
+    }
+    
+    func save() {
+        do {
+            try context.save()
+        } catch let error {
+            print("Error saving Core Data : \(error.localizedDescription)")
+        }
+        
+    }
+}
+
+class CoreDataRelationshipViewModel : ObservableObject {
+    let manager = CoreDataManager.instance
+    
+}
 
 struct ContentView: View {
+    @StateObject var vm = CoreDataRelationshipViewModel()
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
