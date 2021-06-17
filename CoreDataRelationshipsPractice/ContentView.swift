95925d5ff66f81f39e4af562b20b3e78695d680e
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-17 00:33:14 -0400

    First Test

--- CoreDataRelationshipsPractice/ContentView.swift
+++ CoreDataRelationshipsPractice/ContentView.swift
@@ -35,6 +35,7 @@ class CoreDataManager {
     func save() {
         do {
             try context.save()
+            print("Success save.")
         } catch let error {
             print("Error saving Core Data : \(error.localizedDescription)")
         }
@@ -45,14 +46,40 @@ class CoreDataManager {
 class CoreDataRelationshipViewModel : ObservableObject {
     let manager = CoreDataManager.instance
     
+    @Published var businesses : [BusinessEntity] = []
+    
+    func addBusiness() {
+        let newBusiness = BusinessEntity(context: manager.context)
+        newBusiness.name = "Apple"
+        save()
+    }
+    
+    func save(){
+        manager.save()
+    }
+    
 }
 
 struct ContentView: View {
     @StateObject var vm = CoreDataRelationshipViewModel()
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView{
+                VStack(spacing : 20){
+                    Button(action: {
+                        vm.addBusiness()
+                    }, label: {
+                        Text("Perform Action")
+                            .foregroundColor(.white)
+                            .frame(height : 55)
+                            .frame(maxWidth : .infinity)
+                            .background(Color.blue.cornerRadius(10))
+                    })
+                }.padding()
+            }
+            .navigationTitle("Relationships")
+        }
     }
 }
 
