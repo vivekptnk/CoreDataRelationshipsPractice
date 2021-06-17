3db26e30fa3f225f83106271daf22b8d5de329c9
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-17 00:06:04 -0400

    Initial Commit

---
+++ CoreDataRelationshipsPractice/CoreDataRelationshipsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataRelationshipsPracticeApp.swift
+//  CoreDataRelationshipsPractice
+//
+//  Created by Vivek Pattanaik on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataRelationshipsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
